10e867522120ef16f39046710a17a0a9744f5b95
nvdinh185 <nvdinh0766777123@gmail.com>
2022-01-20 10:38:40 +0700

    review
    x

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import './App.css';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <HomePage />
   );
 }
 
